50546dc715f0e37079fc56a34b3e1bebbf4de1b3
Fiixrt <71195247+fiixrt@users.noreply.github.com>
2022-11-06 11:52:14 +0100

    Add files via upload
    fix and local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-const id = "G01-dsc";
-const version = "1.1.2";
-
 $("input").keyup(function (e) {
   console.log(e.key);
   if (e.key === "Enter") {
@@ -9,9 +6,6 @@ $("input").keyup(function (e) {
   }
 });
 
-$(".profil-logo").click(function () {
-  console.log("Le as dado click al logo!");
-});
 
 document.querySelector("#note").value = localStorage.getItem("Note");
 document.querySelector("#note").addEventListener("keyup", (e) => localStorage.setItem("Note", e.target.value));
